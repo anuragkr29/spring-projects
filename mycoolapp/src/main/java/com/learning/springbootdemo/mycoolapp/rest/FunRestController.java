7d9f02f8c12d5b648b05fd61865cceef39dde31c
Anurag Kumar <anuragkr29@gmail.com>
2022-08-30 18:43:25 -0500

    Initial commit

---
+++ mycoolapp/src/main/java/com/learning/springbootdemo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,15 @@
+package com.learning.springbootdemo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class FunRestController {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World! Local time is " + LocalDateTime.now();
+    }
+}
